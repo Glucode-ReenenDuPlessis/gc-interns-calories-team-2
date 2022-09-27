ca77748e1d639ac31b0eebb317b57eec074886f1
Sthembiso Ndhlazi <ndhlazisthe@gmail.com>
2022-09-27 17:00:30 +0200

    update picker

--- CalorieBuddy/CalorieBuddy/Controller/DetailViewController.swift
+++ CalorieBuddy/CalorieBuddy/Controller/DetailViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController,UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var itemImage: UIView!
     @IBOutlet weak var itemsImageView: UIImageView!
@@ -23,8 +23,13 @@ class DetailViewController: UIViewController {
     
     @IBOutlet weak var nutrientLabel4: UILabel!
     
+    @IBOutlet weak var picker: UIPickerView!
     
     var selectedFood:Hint?
+    
+    var pickerData = ["Breakfast","Lunch", "Supper"]
+    var selectedCategory: String?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +45,8 @@ class DetailViewController: UIViewController {
         nutrientLabel2.text = "\(selectedFood!.food.nutrients.procnt ?? 0.00)"
         nutrientLabel3.text = "\(selectedFood!.food.nutrients.chocdf ?? 0.00)"
         nutrientLabel4.text = "\(selectedFood!.food.nutrients.fat ?? 0.00)"
-        
+        self.picker.delegate = self
+        self.picker.dataSource = self
     }
     
 
@@ -48,5 +54,18 @@ class DetailViewController: UIViewController {
         print(selectedFood!.food.label)
     }
     
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedCategory = pickerData[row]
+    }
 }
