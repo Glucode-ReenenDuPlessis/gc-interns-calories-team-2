605def313db423decc08e11a8176581ef18b8ab4
Sibusiso <sibusisom@glucode.com>
2022-09-28 09:55:51 +0200

    Complete new user core data method

--- CalorieBuddy/CalorieBuddy/Controller/ProfileViewController.swift
+++ CalorieBuddy/CalorieBuddy/Controller/ProfileViewController.swift
@@ -17,6 +17,8 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var limit: UITextField!
     
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let dbh = DatabaseHandler()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,35 +28,13 @@ class ProfileViewController: UIViewController {
 
     @IBAction func addInfoPressed(_ sender: UIButton) {
         
-        let newUser = User(context: context)
+        
         
         if let userName = name.text, let userAge = age.text, let userHeight = height.text, let userWeight = weight.text, let userLimit = limit.text {
             
-            let ageInt = Int16(userAge)!
-            let heightDouble = Double(userHeight)!
-            let weightDouble = Double(userWeight)!
-            let limitDouble = Double(userLimit)!
-            
-            newUser.userName = userName
-            newUser.userAge = ageInt
-            newUser.userHeight = heightDouble
-            newUser.userWeight = weightDouble
-            newUser.caloryLimit = limitDouble
-            
-            saveData()
-            print("Saved successfully")
+            dbh.addUser(userName: userName, userAge: userAge, userHeight: userHeight, userWeight: userWeight, userLimit: userLimit)
         }
         
     }
-    
-    func saveData() {
-       
-        do {
-            try context.save()
-        } catch {
-            print("Error saving context: \(error)")
-        }
-    }
-    
 
 }
--- CalorieBuddy/CalorieBuddy/Model/DatabaseHandler.swift
+++ CalorieBuddy/CalorieBuddy/Model/DatabaseHandler.swift
@@ -8,12 +8,47 @@
 import UIKit
 import CoreData
 
-let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-
-func saveTasks() {
-    do {
-        try context.save()
-    } catch {
-        print("Error saving task: \(error)")
+class DatabaseHandler {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    func saveTasks() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving task: \(error)")
+        }
+    }
+    
+    func saveData() {
+       
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error)")
+        }
+    }
+    
+    
+    func addUser(userName: String, userAge: String, userHeight: String, userWeight: String, userLimit: String) {
+        
+        let newUser = User(context: context)
+        
+        let ageInt = Int16(userAge)!
+        let heightDouble = Double(userHeight)!
+        let weightDouble = Double(userWeight)!
+        let limitDouble = Double(userLimit)!
+        
+        print(ageInt)
+        print(heightDouble)
+//        newUser.userName = userName
+//        newUser.userAge = ageInt
+//        newUser.userHeight = heightDouble
+//        newUser.userWeight = weightDouble
+//        newUser.caloryLimit = limitDouble
+//
+//        saveData()
+        print("Saved successfully")
     }
 }
+
