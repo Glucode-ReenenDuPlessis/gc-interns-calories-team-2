20868fd3ddf25ba65d32c20d3e7f35062c34b162
Sibusiso <sibusisom@glucode.com>
2022-09-28 13:21:20 +0200

    Start edit function

--- CalorieBuddy/CalorieBuddy/Controller/ProfileViewController.swift
+++ CalorieBuddy/CalorieBuddy/Controller/ProfileViewController.swift
@@ -28,32 +28,33 @@ class ProfileViewController: UIViewController {
 
     @IBAction func addInfoPressed(_ sender: UIButton) {
         
-        let editedData = dbh.getUserInfo()
-        for user in editedData {
-            print(user.userName)
-            print(user.userAge)
-            print(user.userWeight)
-        }
-//        if let userName = name.text, let userAge = age.text, let userHeight = height.text, let userWeight = weight.text, let userLimit = limit.text {
-//
-//            let editedData = dbh.getUserInfo()
-//
-//            let ageInt = Int16(userAge)!
-//            let heightDouble = Double(userHeight)!
-//            let weightDouble = Double(userWeight)!
-//            let limitDouble = Double(userLimit)!
-//
-//            for user in editedData {
-//                user.setValue(userName, forKey: "userName")
-//                user.setValue(ageInt, forKey: "userAge")
-//                user.setValue(heightDouble, forKey: "userHeight")
-//                user.setValue(weightDouble, forKey: "userWeight")
-//                user.setValue(limitDouble, forKey: "caloryLimit")
-//            }
-//
-//            dbh.saveData()
-//
+//        let editedData = dbh.getUserInfo()
+//        for user in editedData {
+//            print(user.userName)
+//            print(user.userAge)
+//            print(user.userWeight)
 //        }
+        if let userName = name.text, let userAge = age.text, let userHeight = height.text, let userWeight = weight.text, let userLimit = limit.text {
+
+            let editedData = dbh.getUserInfo()
+
+            let ageInt = Int16(userAge)!
+            let heightDouble = Double(userHeight)!
+            let weightDouble = Double(userWeight)!
+            let limitDouble = Double(userLimit)!
+
+            for user in editedData {
+                user.setValue(userName, forKey: "userName")
+                user.setValue(ageInt, forKey: "userAge")
+                user.setValue(heightDouble, forKey: "userHeight")
+                user.setValue(weightDouble, forKey: "userWeight")
+                user.setValue(limitDouble, forKey: "caloryLimit")
+            }
+
+            dbh.saveData()
+            print("successfully edited")
+
+        }
         
     }
 
--- CalorieBuddy/CalorieBuddy/Model/DatabaseHandler.swift
+++ CalorieBuddy/CalorieBuddy/Model/DatabaseHandler.swift
@@ -56,8 +56,42 @@ class DatabaseHandler {
         print("Saved successfully")
     }
     
-    func editUser(userName: String?, userAge: String?, userHeight: String?, userWeight: String?, userLimit: String?) {
+    func editUser(userName: String, userAge: String, userHeight: String, userWeight: String, userLimit: String) {
         
+//        if let name = userName, let age = userAge, let height = userHeight, let weight = userWeight, let limit = userLimit {
+
+//            let editedData = getUserInfo()
+//
+//            for user in editedData {
+//                if userName != nil {
+//                    user.setValue(userName, forKey: "userName")
+//                } else {
+//                    user.setValue(user.userName, forKey: "userName")
+//                }
+//                let age = Int16(user.userAge)
+                
+//                let ageInt = Int16(userAge ?? <#default value#>)!
+//                let heightDouble = Double(userHeight ?? <#default value#>)!
+//                let weightDouble = Double(userWeight ?? <#default value#>)!
+//                let limitDouble = Double(limit)!
+                
+//                print(user.userName ?? user.userName)
+//                print(user.userAge)
+//                print(user.userHeight)
+//                print(user.userWeight)
+//                print(user.caloryLimit)
+//                user.setValue(ageInt, forKey: "userAge")
+//                user.setValue(heightDouble, forKey: "userHeight")
+//                user.setValue(weightDouble, forKey: "userWeight")
+//                user.setValue(limitDouble, forKey: "caloryLimit")
+//            }
+
+//            saveData()
+//            print("successfully edited")
+
+//        } else {
+//            print("Failed to save")
+//        }
     }
 }
 
