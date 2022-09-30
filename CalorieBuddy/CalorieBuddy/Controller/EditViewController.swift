2e525790d909a361d5fc221a3a9480a7865bb2bf
Sibusiso <sibusisom@glucode.com>
2022-09-30 09:55:54 +0200

    Core data profile complete

--- CalorieBuddy/CalorieBuddy/Controller/EditViewController.swift
+++ CalorieBuddy/CalorieBuddy/Controller/EditViewController.swift
@@ -9,31 +9,45 @@ import UIKit
 
 class EditViewController: UIViewController {
     
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var age: UITextField!
+    
+    @IBOutlet weak var height: UITextField!
+    
+    @IBOutlet weak var weight: UITextField!
+    
+    @IBOutlet weak var limit: UITextField!
+    
+    @IBOutlet weak var idealWeight: UITextField!
+    
+    
     let dbh = DatabaseHandler()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        printUser()
         
+        editUser()
     }
     
-    func printUser() {
+    func editUser() {
         let user = dbh.getUserInfo()
-        for eachUser in user {
-            print(eachUser.userName)
-            print(eachUser.userHeight)
+        
+        for user in user {
+            name.placeholder = user.userName
+            age.text = String(user.userAge)
+            height.text = String(user.userHeight)
+            weight.text = String(user.userWeight)
+            limit.text = String(user.caloryLimit)
+            idealWeight.text = String(user.idealUserWeight)
         }
+        
     }
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func CalculatePressed(_ sender: UIButton) {
+        dbh.editUser(userAge: age.text!, userHeight: height.text!, userWeight: weight.text!, userLimit: limit.text!)
     }
-    */
+    
 
 }
--- CalorieBuddy/CalorieBuddy/Model/DatabaseHandler.swift
+++ CalorieBuddy/CalorieBuddy/Model/DatabaseHandler.swift
@@ -59,42 +59,25 @@ class DatabaseHandler {
         print("Saved successfully")
     }
     
-    func editUser(userName: String, userAge: String, userHeight: String, userWeight: String, userLimit: String) {
-        
-//        if let name = userName, let age = userAge, let height = userHeight, let weight = userWeight, let limit = userLimit {
+    func editUser(userAge: String, userHeight: String, userWeight: String, userLimit: String) {
+            let editedData = getUserInfo()
 
-//            let editedData = getUserInfo()
-//
-//            for user in editedData {
-//                if userName != nil {
-//                    user.setValue(userName, forKey: "userName")
-//                } else {
-//                    user.setValue(user.userName, forKey: "userName")
-//                }
-//                let age = Int16(user.userAge)
+            for user in editedData {
+
+                let ageInt = Int16(userAge)!
+                let heightDouble = Double(userHeight)!
+                let weightDouble = Double(userWeight)!
+                let limitDouble = Double(userLimit)!
                 
-//                let ageInt = Int16(userAge ?? <#default value#>)!
-//                let heightDouble = Double(userHeight ?? <#default value#>)!
-//                let weightDouble = Double(userWeight ?? <#default value#>)!
-//                let limitDouble = Double(limit)!
-                
-//                print(user.userName ?? user.userName)
-//                print(user.userAge)
-//                print(user.userHeight)
-//                print(user.userWeight)
-//                print(user.caloryLimit)
-//                user.setValue(ageInt, forKey: "userAge")
-//                user.setValue(heightDouble, forKey: "userHeight")
-//                user.setValue(weightDouble, forKey: "userWeight")
-//                user.setValue(limitDouble, forKey: "caloryLimit")
-//            }
+                user.setValue(ageInt, forKey: "userAge")
+                user.setValue(heightDouble, forKey: "userHeight")
+                user.setValue(weightDouble, forKey: "userWeight")
+                user.setValue(limitDouble, forKey: "caloryLimit")
+            }
 
-//            saveData()
-//            print("successfully edited")
+            saveData()
+            print("successfully edited")
 
-//        } else {
-//            print("Failed to save")
-//        }
     }
 }
 
